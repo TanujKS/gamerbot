f400eb8b7e03439fbd4669dcbca03ebaa6440632
TanujKS <tanujsiripurapu@gmail.com>
2020-12-11 11:22:43 -0800

    Minor edits to env vars

--- bot/main.py
+++ bot/main.py
@@ -1373,7 +1373,6 @@ async def hypixel(ctx, player):
 async def bedwars(ctx, player, *mode):
     data = requests.get(f"https://api.hypixel.net/player?key={HYPIXEL_KEY}&name={player}").json()
     if not data['player']:
-        print(data['player'])
         return await ctx.send(f"{player} has not played Bedwars")
     if len(mode) == 0:
             try:

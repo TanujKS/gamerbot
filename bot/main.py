dcf8b22add3f9e925c329b3b47d3dd225dde8792
TanujKS <tanujsiripurapu@gmail.com>
2020-12-11 11:12:10 -0800

    Minor edits to env vars

--- bot/main.py
+++ bot/main.py
@@ -44,6 +44,9 @@ emojis = ["1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣", "6️⃣", "7�
 teams = ["Team 1", "Team 2", "Team 3", "Team 4", "Team 5", "Team 6", "Team 7", "Team 8"]
 ezmessages = ["Wait... This isn't what I typed!", "Anyone else really like Rick Astley?", "Hey helper, how play game?", "Sometimes I sing soppy, love songs in the car.", "I like long walks on the beach and playing Hypixel", "Please go easy on me, this is my first game!", "You're a great person! Do you want to play some Hypixel games with me?", "In my free time I like to watch cat videos on Youtube", "When I saw the witch with the potion, I knew there was trouble brewing.", "If the Minecraft world is infinite, how is the sun spinning around it?", "Hello everyone! I am an innocent player who loves everything Hypixel.", "Plz give me doggo memes!", "I heard you like Minecraft, so I built a computer in Minecraft in your Minecraft so you can Minecraft while you Minecraft", "Why can't the Ender Dragon read a book? Because he always starts at the End.", "Maybe we can have a rematch?", "I sometimes try to say bad things then this happens :(", "Behold, the great and powerful, my magnificent and almighty nemisis!", "Doin a bamboozle fren.", "Your clicks per second are godly.", "What happens if I add chocolate milk to macaroni and cheese?", "Can you paint with all the colors of the wind", "Blue is greener than purple for sure", "I had something to say, then I forgot it.", "When nothing is right, go left.", "I need help, teach me how to play!", "Your personality shines brighter than the sun.", "You are very good at the game friend.", "I like pineapple on my pizza", "I like pasta, do you prefer nachos?", "I like Minecraft pvp but you are truly better than me!", "I have really enjoyed playing with you! <3", "ILY <3", "Pineapple doesn't go on pizza!", "Lets be friends instead of fighting okay?"]
 HYPIXEL_KEY = os.environ.get('HYPIXEL_KEY')
+TWITCH_CLIENT_ID = os.environ.get('TWITCH_CLIENT_ID')
+YT_KEY = os.environ.get('YT_KEY')
+TWITCH_AUTH = os.environ.get('TWITCH_AUTH')
 
 async def is_vanilla(ctx):
     return ctx.guild.id == 698735288947834900
@@ -1630,7 +1633,7 @@ async def fortnite(ctx, player):
 
 @bot.command()
 async def twitch(ctx, channel):
-    user = requests.get(f"https://api.twitch.tv/helix/users?login={channel}", headers={"client-id":f"{os.environ.get('TWITCH_CLIENT_ID')}", "Authorization":f"{os.environ.get('TWITCH_AUTH')}"}).json()
+    user = requests.get(f"https://api.twitch.tv/helix/users?login={channel}", headers={"client-id":f"{TWITCH_CLIENT_ID}", "Authorization":f"{TWITCH_AUTH}"}).json()
     if not user['data']:
         return await ctx.send("Invalid channel")
     try:
@@ -1643,9 +1646,9 @@ async def twitch(ctx, channel):
         embed.add_field(name="Channel Type", value=data['broadcaster_type'].capitalize(), inline=False)
         embed.add_field(name="Channel Description", value=data['description'], inline=False)
         embed.add_field(name="Views", value=data['view_count'], inline=True)
-        followers = requests.get(f"https://api.twitch.tv/helix/users/follows?to_id={data['id']}", headers={"client-id":f"{os.environ.get('TWITCH_CLIENT_ID')}", "Authorization":f"{os.environ.get('TWITCH_AUTH')}"}).json()
+        followers = requests.get(f"https://api.twitch.tv/helix/users/follows?to_id={data['id']}", headers={"client-id":f"{TWITCH_CLIENT_ID}", "Authorization":f"{TWITCH_AUTH}"}).json()
         embed.add_field(name="Followers", value=followers['total'], inline=True)
-        stream = requests.get(f"https://api.twitch.tv/helix/search/channels?query={channel}/", headers={"client-id":f"{os.environ.get('TWITCH_CLIENT_ID')}", "Authorization":f"{os.environ.get('TWITCH_AUTH')}"}).json()
+        stream = requests.get(f"https://api.twitch.tv/helix/search/channels?query={channel}/", headers={"client-id":f"{TWITCH_CLIENT_ID}", "Authorization":f"{TWITCH_AUTH}"}).json()
         for x in stream['data']:
             is_live = x['is_live']
             break
@@ -1671,11 +1674,11 @@ async def youtube(ctx, *channelarg):
         channel = f"{channel} {x}"
     channel = channel.replace(" ", "%20")
     channel = channel[3:]
-    data = requests.get(f"https://youtube.googleapis.com/youtube/v3/search?part=snippet&q={channel}&type=channel&key={os.environ.get('YT_KEY')}").json()
+    data = requests.get(f"https://youtube.googleapis.com/youtube/v3/search?part=snippet&q={channel}&type=channel&key={YT_KEY}").json()
     if not data['items']:
         return await ctx.send("Invalid channel")
     channel_id = ((data['items'])[0]['snippet']['channelId'])
-    stats = requests.get(f"https://www.googleapis.com/youtube/v3/channels?part=statistics&id={channel_id}&key={os.environ.get('YT_KEY')}").json()
+    stats = requests.get(f"https://www.googleapis.com/youtube/v3/channels?part=statistics&id={channel_id}&key={YT_KEY}").json()
     embed = discord.Embed(title=f"YouTube statistics for {data['items'][0]['snippet']['title']}", description=f"https://www.youtube.com/channel/{channel_id}", color=0xff0000)
     embed.add_field(name="Channel Name:", value=data['items'][0]['snippet']['title'], inline=True)
     embed.add_field(name="Channel ID:", value=channel_id, inline=True)

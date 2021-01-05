67f21034110779542aae3247fe7ff51e3396e60b
TanujKS <tanujsiripurapu@gmail.com>
2021-01-04 21:53:17 -0800

    Working on integrating Redis

--- bot/main.py
+++ bot/main.py
@@ -23,9 +23,31 @@ from mojang import MojangUser
 from mojang.exceptions import SecurityAnswerError
 from mojang.exceptions import LoginError
 from decouple import config
+import redis
 
 bot = commands.Bot(command_prefix='?', intents=discord.Intents.all())
 bot.remove_command('help')
+
+
+HYPIXEL_KEY = os.environ.get('HYPIXEL_KEY')
+TWITCH_CLIENT_ID = os.environ.get('TWITCH_CLIENT_ID')
+YT_KEY = os.environ.get('YT_KEY')
+TWITCH_AUTH = os.environ.get('TWITCH_AUTH')
+TOKEN = os.environ.get("TOKEN")
+REDIS_URL = (os.environ.get("REDIS_URL"))
+KEYS = [HYPIXEL_KEY, TWITCH_CLIENT_ID, HYPIXEL_KEY, YT_KEY, TWITCH_AUTH, REDIS_URL]
+
+if  any(v is None for v in KEYS):
+    print("Using .env vars")
+    HYPIXEL_KEY = config("HYPIXEL_KEY")
+    TWITCH_CLIENT_ID = config("TWITCH_CLIENT_ID")
+    YT_KEY = config("YT_KEY")
+    TWITCH_AUTH = config("TWITCH_AUTH")
+    TOKEN = config("TOKEN")
+    REDIS_URL = config("REDIS_URL")
+
+r = redis.from_url(REDIS_URL)
+
 
 bedwarsModes = {("solos", "solo", "ones"): "eight_one", ("doubles", "double", "twos"): "eight_two", ("3s", "triples", "threes", "3v3v3v3"): "four_three", ("4s", "4v4v4v4", "quadruples", "fours"): "four_four", "4v4": "two_four"}
 skywarsModes = {("solo normal", "solos normal"): "solos normal", ("solo insane", "solos insane"): "solos insane", ("teams normal", "team normal"): "teams normal", ("teams insane", "team insane"): "teams insane"}
@@ -37,22 +59,11 @@ moves = ["rock", "paper", "scissors"]
 emojis = ["1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣", "6️⃣", "7️⃣", "8️⃣"]
 teams = ["Team 1", "Team 2", "Team 3", "Team 4", "Team 5", "Team 6", "Team 7", "Team 8"]
 ezmessages = ["Wait... This isn't what I typed!", "Anyone else really like Rick Astley?", "Hey helper, how play game?", "Sometimes I sing soppy, love songs in the car.", "I like long walks on the beach and playing Hypixel", "Please go easy on me, this is my first game!", "You're a great person! Do you want to play some Hypixel games with me?", "In my free time I like to watch cat videos on Youtube", "When I saw the witch with the potion, I knew there was trouble brewing.", "If the Minecraft world is infinite, how is the sun spinning around it?", "Hello everyone! I am an innocent player who loves everything Hypixel.", "Plz give me doggo memes!", "I heard you like Minecraft, so I built a computer in Minecraft in your Minecraft so you can Minecraft while you Minecraft", "Why can't the Ender Dragon read a book? Because he always starts at the End.", "Maybe we can have a rematch?", "I sometimes try to say bad things then this happens :(", "Behold, the great and powerful, my magnificent and almighty nemisis!", "Doin a bamboozle fren.", "Your clicks per second are godly.", "What happens if I add chocolate milk to macaroni and cheese?", "Can you paint with all the colors of the wind", "Blue is greener than purple for sure", "I had something to say, then I forgot it.", "When nothing is right, go left.", "I need help, teach me how to play!", "Your personality shines brighter than the sun.", "You are very good at the game friend.", "I like pineapple on my pizza", "I like pasta, do you prefer nachos?", "I like Minecraft pvp but you are truly better than me!", "I have really enjoyed playing with you! <3", "ILY <3", "Pineapple doesn't go on pizza!", "Lets be friends instead of fighting okay?"]
-blackListed = []
+
+blacklisted = r.get('blacklisted')
+
 socialMediaLinks = {}
 guildInfo = {}
-HYPIXEL_KEY = os.environ.get('HYPIXEL_KEY')
-TWITCH_CLIENT_ID = os.environ.get('TWITCH_CLIENT_ID')
-YT_KEY = os.environ.get('YT_KEY')
-TWITCH_AUTH = os.environ.get('TWITCH_AUTH')
-TOKEN = os.environ.get("TOKEN")
-KEYS = [HYPIXEL_KEY, TWITCH_CLIENT_ID, HYPIXEL_KEY, YT_KEY, TWITCH_AUTH]
-if  any(v is None for v in KEYS):
-    print("Using .env vars")
-    HYPIXEL_KEY = config("HYPIXEL_KEY")
-    TWITCH_CLIENT_ID = config("TWITCH_CLIENT_ID")
-    YT_KEY = config("YT_KEY")
-    TWITCH_AUTH = config("TWITCH_AUTH")
-    TOKEN = config("TOKEN")
 
 def multi_key_dict_get(d, k):
     for keys, v in d.items():
@@ -60,11 +71,7 @@ def multi_key_dict_get(d, k):
             return v
     return None
 
-def convertPermtoEmoji(member, perm):
-    if getattr(member.guild_permissions, perm) is True:
-        return "✅"
-    if getattr(member.guild_permissions, perm) is False:
-        return "❌"
+
 
 def convertBooltoStr(bool):
     if bool is True:
@@ -377,7 +384,7 @@ async def help(ctx, *category):
         embed.add_field(name="Game Stats Commands (?help stats)", value="Commands to see Minecraft player's stats", inline=False)
         embed.add_field(name="Miscellaneous Commands (?help misc)", value="All other commands I can do!", inline=False)
         embed.add_field(name="APIs (?help apis)", value=f"APIs used by the {str(bot.user)}", inline=False)
-        embed.set_footer(text=f"{str(bot.user)} is a bot created and maintained by tanju_shorty#7767")
+        embed.set_footer(text=f"{str(bot.user)} is a bot created and maintained by tanju_shorty#7767 \nThe full code for GamerBot can be found at https://github.com/TanujKS/gamerbot")
     elif category[0] == "VC":
         embed=discord.Embed(title="VC Commands", description="Commands I can do to help you manage your voice channels", color=0xff0000)
         embed.add_field(name="?mute (member or 'all' or 'channel-all')", value="Server mutes a member. 'channel-all' mutes all people in the channel you are currently in while 'all' mutes everyone a voice channel in the server. (Requires permission Mute Members)", inline=False)
@@ -486,7 +493,8 @@ async def speak(ctx, *message):
         return await ctx.send(f"Role {guildInfo[ctx.guild.id]['TTVCrole']} is required to use TTVC")
     fullmessage = f"{ctx.author.name} says "
     for messageVar in message:
-        fullmessage = f"{fullmessage} {messageVar}"
+        fullmessage += ' '
+        fullmessage += messageVar
     if ctx.guild.voice_client:
         vc = ctx.guild.voice_client
     elif ctx.author.voice:
@@ -503,11 +511,13 @@ async def speak(ctx, *message):
             pass
 
 
-@bot.command()
+#@bot.command()
 async def twitchtrack(ctx, channel):
     user = requests.get(f"https://api.twitch.tv/helix/users?login={channel}", headers={"client-id":f"{TWITCH_CLIENT_ID}", "Authorization":f"{TWITCH_AUTH}"}).json()
     if not user['data']:
         return await ctx.send("Invalid channel")
+
+
 @bot.command()
 @commands.has_guild_permissions(create_instant_invite=True)
 @commands.bot_has_guild_permissions(create_instant_invite=True)
@@ -524,6 +534,12 @@ async def invite(ctx, *args):
             return await ctx.send("Invalid args. 'max_age', 'max_uses', 'reason' ")
     await ctx.send(f"Invite with a maximum age of {max_age} seconds, {max_uses} maximum uses, and with {reason} as the reason. \n{await ctx.channel.create_invite(max_age=max_age, max_uses=max_uses, reason=reason)}")
 
+
+def convertPermtoEmoji(member, perm):
+    if getattr(member.guild_permissions, perm) is True:
+        return "✅"
+    if getattr(member.guild_permissions, perm) is False:
+        return "❌"
 
 @bot.command()
 @commands.guild_only()

1d5d00cb9b0d21d2e416821ba26435c6ec945858
TanujKS <tanujsiripurapu@gmail.com>
2021-01-26 11:27:53 -0800

    Fixed timer commands and added them to help screen

--- bot/main.py
+++ bot/main.py
@@ -49,7 +49,7 @@ moves = ["rock", "paper", "scissors"]
 emojis = ["1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣", "6️⃣", "7️⃣", "8️⃣"]
 teams = ["Team 1", "Team 2", "Team 3", "Team 4", "Team 5", "Team 6", "Team 7", "Team 8"]
 ezmessages = ["Wait... This isn't what I typed!", "Anyone else really like Rick Astley?", "Hey helper, how play game?", "Sometimes I sing soppy, love songs in the car.", "I like long walks on the beach and playing Hypixel", "Please go easy on me, this is my first game!", "You're a great person! Do you want to play some Hypixel games with me?", "In my free time I like to watch cat videos on Youtube", "When I saw the witch with the potion, I knew there was trouble brewing.", "If the Minecraft world is infinite, how is the sun spinning around it?", "Hello everyone! I am an innocent player who loves everything Hypixel.", "Plz give me doggo memes!", "I heard you like Minecraft, so I built a computer in Minecraft in your Minecraft so you can Minecraft while you Minecraft", "Why can't the Ender Dragon read a book? Because he always starts at the End.", "Maybe we can have a rematch?", "I sometimes try to say bad things then this happens :(", "Behold, the great and powerful, my magnificent and almighty nemisis!", "Doin a bamboozle fren.", "Your clicks per second are godly.", "What happens if I add chocolate milk to macaroni and cheese?", "Can you paint with all the colors of the wind", "Blue is greener than purple for sure", "I had something to say, then I forgot it.", "When nothing is right, go left.", "I need help, teach me how to play!", "Your personality shines brighter than the sun.", "You are very good at the game friend.", "I like pineapple on my pizza", "I like pasta, do you prefer nachos?", "I like Minecraft pvp but you are truly better than me!", "I have really enjoyed playing with you! <3", "ILY <3", "Pineapple doesn't go on pizza!", "Lets be friends instead of fighting okay?"]
-
+stopWatches = {}
 
 blackListed = r.lrange("blacklisted", 0, -1)
 for i in range(0, len(blackListed)):
@@ -526,10 +526,9 @@ async def help(ctx, *category):
         embed.add_field(name=f"{prefix}settings", value="Tweak settings for your guild", inline=False)
         embed.add_field(name=f"{prefix}rps (user or 'bot')", value="Challenges a member (or the bot) to Rock Paper Scissors", inline=False)
         embed.add_field(name=f"{prefix}dm (user or role)", value="Useful if you need to DM a large amount of members a message", inline=False)
-        embed.add_field(name=f"{prefix}twitchtrack (twitch_channel) (message to send when streamer goes live)", value="Track Twitch streamers and get notified whenever they stream", inline=False)
-        embed.add_field(name=f"{prefix}deltrack (twitch_channel)", value="Stop tracking a Twitch streamer", inline=False)
-        embed.add_field(name=f"{prefix}twitchtracklist", value="Shows all Twitch streamers that are being tracked", inline=False)
         embed.add_field(name=f"{prefix}donate", value="Information about donating to GamerBot", inline=False)
+        embed.add_field(name=f"{prefix}starttimer", value="Starts a stopwatch", inline=False)
+        embed.add_field(name=f"{prefix}stoptimer", value="Stops a stopwatch", inline=False)
     elif category[0] == "stats":
         embed=discord.Embed(title="Game Stat Commands", description="Commands to see a player's stats in various games", color=0xff0000)
         embed.add_field(name=f"{prefix}minecraft (minecraft_player)", value="Shows stats about a Minecraft player", inline=False)
@@ -541,6 +540,9 @@ async def help(ctx, *category):
         embed.add_field(name=f"{prefix}duels (minecraft_player) (optional_mode)", value="Shows stats about a Hypixel Duels player \nOptional modes are classic, uhc, op, sumo, skywars, uhc doubles, combo, bridge", inline=False)
         embed.add_field(name=f"{prefix}fortnite (fortnite_player)", value="Shows stats about a Fortnite player", inline=False)
         embed.add_field(name=f"{prefix}twitch (channel)", value="Shows stats of a Twitch streamer", inline=False)
+        embed.add_field(name=f"{prefix}twitchtrack (twitch_channel) (message to send when streamer goes live)", value="Track Twitch streamers and get notified whenever they stream", inline=False)
+        embed.add_field(name=f"{prefix}deltrack (twitch_channel)", value="Stop tracking a Twitch streamer", inline=False)
+        embed.add_field(name=f"{prefix}twitchtracklist", value="Shows all Twitch streamers that are being tracked", inline=False)
         embed.add_field(name=f"{prefix}youtube (channel)", value="Shows stats of a YouTube channel", inline=False)
         embed.add_field(name=f"{prefix}csgo (id)", value="Shows stats of a CS:GO player", inline=False)
         embed.add_field(name=f"{prefix}csgolink (id)", value="Links your account to a CS:GO ID", inline=False)

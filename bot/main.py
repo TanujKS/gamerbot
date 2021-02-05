c69c93f5791d13ef60001d1c2478dce53229ffb2
TanujKS <tanujsiripurapu@gmail.com>
2021-02-05 15:33:37 -0800

    Fixed bedwars command

--- bot/main.py
+++ bot/main.py
@@ -1534,7 +1534,7 @@ async def bedwars(ctx, *player_and_mode):
             embed.add_field(name="Final Kills:", value=data.get("final_kills_bedwars", 0), inline=True)
             embed.add_field(name="Final Deaths:", value=data.get("final_deaths_bedwars", 0), inline=True)
             embed.add_field(name="Final K/D Rate:", value=getrate(data.get("final_kills_bedwars", 0), data.get("final_deaths_bedwars", 0)), inline=True)
-            embed.add_field(name="Beds Broken:", value=data.get("beds_broken_bedwars", 0), inline=True
+            embed.add_field(name="Beds Broken:", value=data.get("beds_broken_bedwars", 0), inline=True)
             embed.add_field(name="Beds Lost:", value=data.get("beds_lost_bedwars", 0), inline=True)
             embed.add_field(name="B/L Rate:", value=getrate(data.get("beds_broken_bedwars", 0), data.get("beds_lost_bedwars", 0)), inline=True)
     else:

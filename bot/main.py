11cea1e869df959c2e486a32e3e445571e61c234
TanujKS <70826548+TanujKS@users.noreply.github.com>
2021-03-15 11:04:26 -0700

    Major commits to error handling

--- bot/main.py
+++ bot/main.py
@@ -1549,7 +1549,7 @@ async def bedwars(ctx, *player_and_mode):
     rawData = requests.get(f"https://api.hypixel.net/player?key={HYPIXEL_KEY}&uuid={uuid}").json()
     if rawData.get("success") is False:
         raise commands.BadArgument(rawData.get('cause'))
-    if not rawData.get('player') or rawData['player']['stats'].get("Bedwars"):
+    if not rawData.get('player') or not rawData['player']['stats'].get("Bedwars"):
         raise commands.BadArgument(f"{player} has not played Bedwars")
     data = rawData['player']['stats']['Bedwars']
     if len(player_and_mode) < 2:
